99f3fdcfd683d1ab32125392f0659dbf8fc09ced
clp.sampedro@gmail.com <claudialazarapoietsampedro@Claudias-MacBook-Pro.local>
2024-05-29 20:18:24 -0300

    aulas 27 e 28 de maio

---
+++ aula-tads/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="title">Primeiro projeto em ReactJS</h1>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/2/22/Logo_Flamengo_crest_1980-2018.png"
+        alt="logo do flamengo"
+        width={300}
+      />
+    </div>
+  );
+}
+
+export default App;
